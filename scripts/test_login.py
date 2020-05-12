2bd4d0861e1b5588bb3e5b884057f14ffdf08d3d
LeeChaoQ <lichao900602@126.com>
2020-05-13 00:30:29 +0800

    专属商城初始化

---
+++ scripts/test_login.py
@@ -0,0 +1,12 @@
+from ZS_Store.base.base_driver import init_driver
+
+class TestLogin():
+
+    def setup(self):
+        self.driver = init_driver()
+
+    def teardown(self):
+        self.driver.quit()
+
+    def test_login(self):
+        pass
